38debb1f9e800498e38c4bec7998023ff13dcebf
Manuel Ponce <mponcel@gmail.com>
2014-08-09 02:16:02 -0500

    initial commit

---
+++ guardar.php
@@ -0,0 +1,35 @@
+<?php
+error_reporting(E_ALL);ini_set("display_errors", 1);
+include("conexion.php");
+
+$id = $_POST['id'];
+$nombre = $_POST['nombre'];
+$autor = $_POST['autor'];
+$codigo = $_POST['codigo'];
+$lenguaje = $_POST['lenguaje'];
+$descripcion = $_POST['descripcion'];
+$opcion = $_POST['opcion'];
+
+$codigo=htmlentities(addslashes($codigo));
+switch ($opcion) {
+    case "Nuevo":
+        echo "Nuevo";
+		
+		$sql="INSERT INTO codigos (nombre, autor,lenguaje,descripcion,codigo) VALUES ('$nombre', '$autor','$lenguaje','$descripcion','$codigo')";
+		mysql_query($sql);
+		header ("Location: index.php");
+        break;
+    case "Editar":
+        echo "Editar";
+		$sql = "UPDATE codigos SET nombre='$nombre', autor='$autor', codigo='$codigo', lenguaje='$lenguaje', descripcion='$descripcion' WHERE id_codigo = $id";
+		echo $sql;
+		mysql_query($sql);
+		//echo "se ha actualizado";
+		header ("Location: index.php");
+        break;
+    default:
+		echo "Otro";
+		break;
+	
+}
+?>
---
+++ lista.php
@@ -0,0 +1,38 @@
+
+
+<?php
+error_reporting(E_ALL);ini_set("display_errors", 1);
+include("conexion.php");
+
+$sql = "SELECT * from codigos";
+$result = mysql_query($sql);
+
+ 
+echo '<table width="90%" border="0" cellspacing="3" cellpadding="3" align="center">
+  <tr>
+    <td bgcolor="#FFFFFF" class="labelcenter">Nombre</td>
+    <td bgcolor="#FFFFFF" class="labelcenter">Autor</td>
+    <td bgcolor="#FFFFFF" class="labelcenter">Descripción</td>
+    <td bgcolor="#FFFFFF" class="labelcenter">Lenguaje</td>
+    <td bgcolor="#FFFFFF" class="labelcenter">Creado</td>
+	<td bgcolor="#FFFFFF" class="labelcenter">Edición</td>
+  </tr>';
+  while($row = mysql_fetch_array($result)) {
+	echo "<tr>
+    <td class='texto'><a href='codigo.php?id=$row[id_codigo]' class='modal'>$row[nombre]</a></td>
+    <td class='texto'>$row[autor]</td>
+    <td class='texto'>$row[descripcion]</td>
+    <td class='texto'>$row[lenguaje]</td>
+    <td class='texto'>$row[fecha]</td>
+	<td class='texto'><form name='form1' method='post' action='editar.php'>
+  <input type='submit' name='editar' id='editar' value='Editar'>
+  <input name='id' type='hidden' value='$row[id_codigo]'>
+</form>
+</td>
+  </tr>";
+  }
+echo '</table>';
+?>
+
+
+  
